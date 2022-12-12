230e40214099eececba0b1678d2227b0de816a02
tomfantova <tom.fantova@gmail.com>
2022-12-12 09:53:54 +0100

    debug persistore

--- App.tsx
+++ App.tsx
@@ -1,40 +1,40 @@
-import React from 'react'
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
 
-import { Provider } from 'react-redux'
-import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
-import user from './reducers/user'
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import user from "./reducers/user";
 
-import { persistStore, persistReducer } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
-import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const reducers = combineReducers({ user })
-const persistConfig = { key: 'monnouveaupanier', storage }
+const reducers = combineReducers({ user });
+const persistConfig = { key: "monnouveaupanier", storage: AsyncStorage };
 
 const store = configureStore({
   reducer: persistReducer(persistConfig, reducers),
-  middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false })
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
 
-const persistor = persistStore(store)
+const persistor = persistStore(store);
 
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import WelcomeScreen from './screens/WelcomeScreen';
-import GuidesScreen from './screens/GuidesScreen';
-import BookmarksScreen from './screens/BookmarksScreen';
-import OffersScreen from './screens/OffersScreen'
-import ListsScreen from './screens/ListsScreen';
+import WelcomeScreen from "./screens/WelcomeScreen";
+import GuidesScreen from "./screens/GuidesScreen";
+import BookmarksScreen from "./screens/BookmarksScreen";
+import OffersScreen from "./screens/OffersScreen";
+import ListsScreen from "./screens/ListsScreen";
 
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
-
-  const Stack = createNativeStackNavigator()
-  const Tab = createBottomTabNavigator()
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
 
   const TabNavigator = () => {
     return (
@@ -42,22 +42,22 @@ export default function App() {
         screenOptions={({ route }) => ({
           tabBarShowLabel: false,
           tabBarIcon: ({ color, size }) => {
-            let iconName = ''
+            let iconName = "";
 
-            if (route.name === 'MNP') {
-              iconName = 'map-pin'
-            } else if (route.name === 'Favoris') {
-              iconName = 'map-pin'
-            } else if (route.name === 'Offres') {
-              iconName = 'map-pin'
-            } else if (route.name === 'Listes') {
-              iconName = 'map-pin'
+            if (route.name === "MNP") {
+              iconName = "map-pin";
+            } else if (route.name === "Favoris") {
+              iconName = "map-pin";
+            } else if (route.name === "Offres") {
+              iconName = "map-pin";
+            } else if (route.name === "Listes") {
+              iconName = "map-pin";
             }
 
-            return <FontAwesome name={iconName} size={size} color={color} />
+            return <FontAwesome name={iconName} size={size} color={color} />;
           },
-          tabBarActiveTintColor: 'orange',
-          tabBarInactiveTintColor: 'grey',
+          tabBarActiveTintColor: "orange",
+          tabBarInactiveTintColor: "grey",
           headerShown: true,
         })}
       >
@@ -66,17 +66,19 @@ export default function App() {
         <Tab.Screen name="Offres" component={OffersScreen} />
         <Tab.Screen name="Listes" component={ListsScreen} />
       </Tab.Navigator>
-    )
-  }
+    );
+  };
 
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StatusBar style="auto" />
         <NavigationContainer>
-          <Stack.Navigator screenOptions={{
-            headerShown: false,
-          }}>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
             <Stack.Screen name="Bienvenue" component={WelcomeScreen} />
             <Stack.Screen name="TabNavigator" component={TabNavigator} />
           </Stack.Navigator>
--- reducers/user.ts
+++ reducers/user.ts
@@ -1,48 +1,51 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export type UserState = {
-    value: {
-        firstname: string | null,
-        lastname: string | null,
-        email: string | null,
-        token: string | null,
-        preferences: {},
-        bookmarks: {
-            interviews: string[],
-            generalities: string[],
-            products: string[],
-            labels: string[],
-        },
-        lists: [],
-    }
-}
+  value: {
+    firstname: string | null;
+    lastname: string | null;
+    email: string | null;
+    token: string | null;
+    preferences: {};
+    bookmarks: {
+      interviews: string[];
+      generalities: string[];
+      products: string[];
+      labels: string[];
+    };
+    lists: [];
+  };
+};
 
 const initialState: UserState = {
-    value: {
-        firstname: null,
-        lastname: null,
-        email: null,
-        token: null,
-        preferences: {},
-        bookmarks: {
-            interviews: [],
-            generalities: [],
-            products: [],
-            labels: [],
-        },
-        lists: [],
-    }
-}
+  value: {
+    firstname: null,
+    lastname: null,
+    email: null,
+    token: null,
+    preferences: {},
+    bookmarks: {
+      interviews: [],
+      generalities: [],
+      products: [],
+      labels: [],
+    },
+    lists: [],
+  },
+};
 
 export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        connectUser: (state: UserState, action: PayloadAction<UserState["value"]>) => {
-            state.value = action.payload
-        }
-    }
-})
+  name: "user",
+  initialState,
+  reducers: {
+    connectUser: (
+      state: UserState,
+      action: PayloadAction<UserState["value"]>
+    ) => {
+      state.value = action.payload;
+    },
+  },
+});
 
-export const { connectUser } = userSlice.actions
-export default userSlice.reducer
+export const { connectUser } = userSlice.actions;
+export default userSlice.reducer;
